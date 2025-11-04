a53f1a36b31e9955b38e4bff655fc83988f87252
Twinkle-thomas <twinklethomas52@gmail.com>
2025-11-04 16:41:44 +1300

    Added activity creation and listing the activities

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,7 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from .views import RegisterView, LogoutView
+from .views import ActivityCreateView, ActivityListView
 
 urlpatterns = [
     path('auth/register/', RegisterView.as_view(), name='register'),
@@ -9,4 +10,7 @@ urlpatterns = [
     path('auth/logout/', LogoutView.as_view(), name='logout'),
     # ✅ Refresh token endpoint
     path('auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+path('activities/', ActivityListView.as_view(), name='activity-list'),
+    path('activities/create/', ActivityCreateView.as_view(), name='activity-create'),
+
 ]
